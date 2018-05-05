9ac6456279e326e167189362f2144054a7a5d54e
David Waller <david@krawaller.se>
2018-05-05 16:26:43 +0200

    update callbag-mock and tests

--- test.js
+++ test.js
@@ -3,12 +3,9 @@ const makeMockCallbag = require('callbag-mock');
 const mapTo = require('./index');
 
 test('it maps data to a fixed value and hands along terminations', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && history.push([name,dir,t,d]);
-
-  const source = makeMockCallbag('source', true);
+  const source = makeMockCallbag(true);
   const mapToFoo = mapTo('foo');
-  const sink = makeMockCallbag('sink', report);
+  const sink = makeMockCallbag();
 
   mapToFoo(source)(0, sink);
 
@@ -16,32 +13,28 @@ test('it maps data to a fixed value and hands along terminations', t => {
   source.emit(1, 'baz');
   source.emit(2, 'error');
 
-  t.deepEqual(history, [
-    ['sink', 'body', 1, 'foo'],
-    ['sink', 'body', 1, 'foo'],
-    ['sink', 'body', 2, 'error'],
-  ], 'sink gets terminations and mapped values');
-
+  t.deepEqual(
+    sink.getReceivedData(),
+    ['foo', 'foo'],
+    'data was mapped'
+  );
+  t.ok(!sink.checkConnection(), 'termination was passed along');
   t.end();
 });
 
 test('it passes requests back up', t => {
-  let history = [];
-  const report = (name,dir,t,d) => t !== 0 && history.push([name,dir,t,d]);
-
-  const source = makeMockCallbag('source', report, true);
+  const source = makeMockCallbag(true);
   const mapToFoo = mapTo('foo');
-  const sink = makeMockCallbag('sink', report);
+  const sink = makeMockCallbag();
 
   mapToFoo(source)(0, sink);
 
   sink.emit(1);
   sink.emit(2);
 
-  t.deepEqual(history, [
-    ['source', 'talkback', 1, undefined],
-    ['source', 'talkback', 2, undefined],
-  ], 'source gets requests from sink');
+  const [init,...messages] = source.getMessages();
 
+  t.equal(init[0], 0, 'source got init request');
+  t.deepEqual(messages, [[1, undefined], [2, undefined]], 'source got messages');
   t.end();
 });
